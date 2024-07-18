802576628af84712462fc32a08e69b5104fc7029
duma <126393227+vinzcyun@users.noreply.github.com>
2024-07-18 19:43:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -165,7 +165,7 @@ async def generate_response(prompt, max_retries=10):
     while retries < max_retries:
         try:
             genai.configure(api_key=get_random_api_key())
-            model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest")
+            model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
             response = await asyncio.to_thread(model.generate_content, prompt, safety_settings=safety_settings)
             return response.text
         except Exception as e:
